28b780bb6a72506524b60316f7cc78d3618cb53c
bajrushivlora <bajrushivlora@gmail.com>
2019-05-27 00:59:19 -0400

    lol

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,34 @@
-document.querySelector('.bar_one').addEventListener("mouseover", email);
+const x = document.querySelector('.bar_one');
+const y = document.querySelector('.email');
+const z = document.querySelector('.bar_two');
+const w = document.querySelector('.linkedin');
 
-function email () {
-    document.querySelector('.bar_one').style.background("--black");
+x.addEventListener("mouseover", hover);
+x.addEventListener("mouseout", hoverOut);
+
+function hover () {
+    x.style.backgroundColor = "#fffc70";
+    x.style.borderColor = "#fffc70";
+    y.style.color = "#000000";
+}
+
+function hoverOut () {
+    x.style.backgroundColor = "transparent";
+    x.style.borderColor = "#ffffff";
+    y.style.color = "#fffc70";
+}
+
+z.addEventListener("mouseover", mouseover);
+z.addEventListener("mouseout", mouseOut);
+
+function mouseover () {
+    z.style.backgroundColor = "#fffc70";
+    z.style.borderColor = "#fffc70";
+    w.style.color = "#000000";
+}
+
+function mouseOut () {
+    z.style.backgroundColor = "transparent";
+    z.style.borderColor = "#ffffff";
+    w.style.color = "#fffc70";
 }
