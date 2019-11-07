a48257ee8df06ee03fa782ca257ec3c76f4b47c0
bajrushivlora <bajrushivlora@gmail.com>
2019-11-07 13:18:30 -0500

    lol

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,7 @@
 var everything = document.querySelectorAll('.flexbox-work_text');
-
-var myArray = []
 var myAnimation = {distance: '150px', duration: 1400};
-
 
 ScrollReveal().reveal(everything, myAnimation);
 
+//'reset: true' will restart the animation when u scroll back up
 
-//opacity
-//easing: 'ease-in'
-//duration: 2500 (how long animations take to complete)
-//distance: 200px
-//reset: true===== restarts the animation when u scroll back up
