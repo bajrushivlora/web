d8cd7ff0c150a5e49be90987b5c1bdc529003925
bajrushivlora <bajrushivlora@gmail.com>
2020-04-20 17:02:48 -0400

    lol

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,10 @@ var myAnimation = {distance: '150px', duration: 1400};
 
 ScrollReveal().reveal(everything, myAnimation);
 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
