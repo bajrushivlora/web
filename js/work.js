d7360356ca4224d83820758b198721fca192fe95
bajrushivlora <bajrushivlora@gmail.com>
2020-09-28 13:15:45 -0400

    designn work page done

--- js/work.js
+++ js/work.js
@@ -1,18 +1,255 @@
-// var circles1;
-// var circles2;
-// var circles3;
-// var circles4;
-// var i;
-// var container = document.querySelector(".flexbox-work_left");
+var one = document.getElementById('1');
+var two = document.getElementById('2');
+var three = document.getElementById('3');
+var four = document.getElementById('4');
+var five = document.getElementById('5');
+var six = document.getElementById('6');
+var seven = document.getElementById('7');
+var eight = document.getElementById('8');
+var nine = document.getElementById('9');
+var ten = document.getElementById('10');
+var eleven = document.getElementById('11');
+var twelve = document.getElementById('12');
+var thirteen = document.getElementById('13');
+var fourteen = document.getElementById('14');
+var fifteen = document.getElementById('15');
+var sixteen = document.getElementById('16');
 
-// for(i = 0; i < 3; i++) {
-//     var circles1 = document.createElement('div');
-//     circles1.classList.add('circular');
-//     container.appendChild(circles1);
-// }
+var text1 = document.querySelector(".text1");
+var text2 = document.querySelector(".text2");
+var text3 = document.querySelector(".text3");
+var text4 = document.querySelector(".text4");
+var text5 = document.querySelector(".text5");
+var text6 = document.querySelector(".text6");
+var text7 = document.querySelector(".text7");
+var text8 = document.querySelector(".text8");
+var text9 = document.querySelector(".text9");
+var text10 = document.querySelector(".text10");
+var text11 = document.querySelector(".text11");
+var text12 = document.querySelector(".text12");
+var text13= document.querySelector(".text13");
+var text14 = document.querySelector(".text14");
+var text15 = document.querySelector(".text15");
+var text16 = document.querySelector(".text16");
 
-// for(i = 0; i < 2; i++) {
-//     var circles2 = document.createElement('div');
-//     circles2.classList.add('circular');
-//     container.appendChild(circles2);
+
+
+function hoverOverOne() {
+    one.style.backgroundColor = 'white';
+    text1.style.color = '#ff413b';
+}
+
+function hoverOutOne() {
+    one.style.backgroundColor = 'transparent';
+    text1.style.color = 'white';
+}
+
+function hoverOverTwo() {
+    two.style.backgroundColor = 'white';
+    text2.style.color = '#ff413b';
+}
+
+function hoverOutTwo() {
+    two.style.backgroundColor = 'transparent';
+    text2.style.color = 'white';
+}
+
+function hoverOverThree() {
+    three.style.backgroundColor = 'white';
+    text3.style.color = '#ff413b';
+}
+
+function hoverOutThree() {
+    three.style.backgroundColor = 'transparent';
+    text3.style.color = 'white';
+}
+
+function hoverOverFour() {
+    four.style.backgroundColor = 'white'; 
+    text4.style.color = '#ff413b';   
+}
+
+function hoverOutFour() {
+    four.style.backgroundColor = 'transparent';
+    text4.style.color = 'white';
+}
+
+function hoverOverFive() {
+    five.style.backgroundColor = 'white'; 
+    text5.style.color = '#ff413b';   
+}
+
+function hoverOutFive() {
+    five.style.backgroundColor = 'transparent';
+    text5.style.color = 'white';
+}
+
+function hoverOverSix() {
+    six.style.backgroundColor = 'white'; 
+    text6.style.color = '#ff413b';   
+}
+
+function hoverOutSix() {
+    six.style.backgroundColor = 'transparent';
+    text6.style.color = 'white';
+}
+
+function hoverOverSeven() {
+    seven.style.backgroundColor = 'white'; 
+    text7.style.color = '#ff413b';   
+}
+
+function hoverOutSeven() {
+    seven.style.backgroundColor = 'transparent';
+    text7.style.color = 'white';
+}
+
+function hoverOverEight() {
+    eight.style.backgroundColor = 'white'; 
+    text8.style.color = '#ff413b';   
+}
+
+function hoverOutEight() {
+    eight.style.backgroundColor = 'transparent';
+    text8.style.color = 'white';
+}
+
+function hoverOverNine() {
+    nine.style.backgroundColor = 'white'; 
+    text9.style.color = '#ff413b';   
+}
+
+function hoverOutNine() {
+    nine.style.backgroundColor = 'transparent';
+    text9.style.color = 'white';
+}
+
+function hoverOverTen() {
+    ten.style.backgroundColor = 'white'; 
+    text10.style.color = '#ff413b';   
+}
+
+function hoverOutTen() {
+    ten.style.backgroundColor = 'transparent';
+    text10.style.color = 'white';
+}
+
+function hoverOverEleven() {
+    eleven.style.backgroundColor = 'white'; 
+    text11.style.color = '#ff413b';   
+}
+
+function hoverOutEleven() {
+    eleven.style.backgroundColor = 'transparent';
+    text11.style.color = 'white';
+}
+
+function hoverOverTwelve() {
+    twelve.style.backgroundColor = 'white'; 
+    text12.style.color = '#ff413b';   
+}
+
+function hoverOutTwelve() {
+    twelve.style.backgroundColor = 'transparent';
+    text12.style.color = 'white';
+}
+
+function hoverOverThirteen() {
+    thirteen.style.backgroundColor = 'white'; 
+    text13.style.color = '#ff413b';   
+}
+
+function hoverOutThirteen() {
+    thirteen.style.backgroundColor = 'transparent';
+    text13.style.color = 'white';
+}
+
+function hoverOverFourteen() {
+    fourteen.style.backgroundColor = 'white'; 
+    text14.style.color = '#ff413b';   
+}
+
+function hoverOutFourteen() {
+    fourteen.style.backgroundColor = 'transparent';
+    text14.style.color = 'white';
+}
+
+function hoverOverFifteen() {
+    fifteen.style.backgroundColor = 'white'; 
+    text15.style.color = '#ff413b';   
+}
+
+function hoverOutFifteen() {
+    fifteen.style.backgroundColor = 'transparent';
+    text13.style.color = 'white';
+}
+
+function hoverOverSixteen() {
+    sixteen.style.backgroundColor = 'white'; 
+    text16.style.color = '#ff413b';   
+}
+
+function hoverOutSixteen() {
+    sixteen.style.backgroundColor = 'transparent';
+    text16.style.color = 'white';
+}
+
+
+one.addEventListener('mouseover', hoverOverOne);
+one.addEventListener('mouseout', hoverOutOne);
+
+two.addEventListener('mouseover', hoverOverTwo);
+two.addEventListener('mouseout', hoverOutTwo);
+
+three.addEventListener('mouseover', hoverOverThree);
+three.addEventListener('mouseout', hoverOutThree);
+
+four.addEventListener('mouseover', hoverOverFour);
+four.addEventListener('mouseout', hoverOutFour);
+
+five.addEventListener('mouseover', hoverOverFive);
+five.addEventListener('mouseout', hoverOutFive);
+
+six.addEventListener('mouseover', hoverOverSix);
+six.addEventListener('mouseout', hoverOutSix);
+
+seven.addEventListener('mouseover', hoverOverSeven);
+seven.addEventListener('mouseout', hoverOutSeven);
+
+eight.addEventListener('mouseover', hoverOverEight);
+eight.addEventListener('mouseout', hoverOutEight);
+
+nine.addEventListener('mouseover', hoverOverNine);
+nine.addEventListener('mouseout', hoverOutNine);
+
+ten.addEventListener('mouseover', hoverOverTen);
+ten.addEventListener('mouseout', hoverOutTen);
+
+eleven.addEventListener('mouseover', hoverOverEleven);
+eleven.addEventListener('mouseout', hoverOutEleven);
+
+twelve.addEventListener('mouseover', hoverOverTwelve);
+twelve.addEventListener('mouseout', hoverOutTwelve);
+
+thirteen.addEventListener('mouseover', hoverOverThirteen);
+thirteen.addEventListener('mouseout', hoverOutThirteen);
+
+fourteen.addEventListener('mouseover', hoverOverFourteen);
+fourteen.addEventListener('mouseout', hoverOutFourteen);
+
+fifteen.addEventListener('mouseover', hoverOverFifteen);
+fifteen.addEventListener('mouseout', hoverOutFifteen);
+
+sixteen.addEventListener('mouseover', hoverOverSixteen);
+sixteen.addEventListener('mouseout', hoverOutSixteen);
+//------------------------
+
+// var deleteLink = document.querySelectorAll('.delete');
+
+// for (var i = 0; i < deleteLink.length; i++) {
+//     deleteLink[i].addEventListener('click', function(event) {
+//         if (!confirm("sure u want to delete " + this.title)) {
+//             event.preventDefault();
+//         }
+//     });
 // }
