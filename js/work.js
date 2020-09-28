ea024939c4c27e229c56ce0d6f6e2d332c8ab6b4
bajrushivlora <bajrushivlora@gmail.com>
2020-09-28 10:32:53 -0400

    work page

---
+++ js/work.js
@@ -0,0 +1,18 @@
+// var circles1;
+// var circles2;
+// var circles3;
+// var circles4;
+// var i;
+// var container = document.querySelector(".flexbox-work_left");
+
+// for(i = 0; i < 3; i++) {
+//     var circles1 = document.createElement('div');
+//     circles1.classList.add('circular');
+//     container.appendChild(circles1);
+// }
+
+// for(i = 0; i < 2; i++) {
+//     var circles2 = document.createElement('div');
+//     circles2.classList.add('circular');
+//     container.appendChild(circles2);
+// }
