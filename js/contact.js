f5f18fdcb49f047e9d64e7bdb1e85c85591316e8
bajrushivlora <bajrushivlora@gmail.com>
2022-03-04 17:19:16 -0500

    finalized

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,4 @@
+var i;
 var one = document.querySelector(".contact-flexbox_bottom1");
 var two = document.querySelector(".contact-flexbox_bottom2");
 var three = document.querySelector(".contact-flexbox_bottom3");
@@ -6,40 +7,40 @@ var link1 = document.getElementById("link1");
 var link2 = document.getElementById("link2");
 var link3 = document.getElementById("link3");
 
-link1.addEventListener("mouseover", changeColor1);
-link2.addEventListener("mouseover", changeColor1);
-link3.addEventListener("mouseover", changeColor1);
+one.addEventListener('mouseover', changeColor1);
+two.addEventListener('mouseover', changeColor2);
+three.addEventListener('mouseover', changeColor3);
 
-link1.addEventListener("mouseout", color1);
-link2.addEventListener("mouseout", color2);
-link3.addEventListener("mouseout", color3);
+// link1.addEventListener("mouseout", color1);
+// link2.addEventListener("mouseout", color2);
+// link3.addEventListener("mouseout", color3);
 
 function changeColor1() {
-    link1.classList.add(".bluetext");
-    one.classList.add(".whitebkgd");
+    link1.style.color = "#5170ed";
+    one.style.background = "white";
 }
 
 function changeColor2() {
-    link2.classList.add(".bluetext");
-    two.classList.add(".whitebkgd");
+    link2.style.color = "#5170ed";
+    two.style.background = "white";
 }
 
 function changeColor3() {
-    link3.classList.add(".bluetext");
-    three.classList.add(".whitebkgd");
+    link3.style.color = "#5170ed";
+    three.style.background = "white";
 }
 
-function color1() {
-    link1.classList.remove(".bluetext");
-    one.classList.remove(".whitebkgd");
-}
+// function color1() {
+//     link1.classList.remove(".bluetext");
+//     one.classList.remove(".whitebkgd");
+// }
 
-function color2() {
-    link2.classList.remove(".bluetext");
-    two.classList.remove(".whitebkgd");
-}
+// function color2() {
+//     link2.classList.remove(".bluetext");
+//     two.classList.remove(".whitebkgd");
+// }
 
-function color3() {
-    link3.classList.remove(".bluetext");
-    three.classList.remove(".whitebkgd");
-}
+// function color3() {
+//     link3.classList.remove(".bluetext");
+//     three.classList.remove(".whitebkgd");
+// }
--- js/scripts-synesthesia.js
+++ js/scripts-synesthesia.js
@@ -1,10 +1,42 @@
+// var plusButton = document.querySelector(".synesthesia-menu_zoomin");
+// var minusButton = document.querySelector(".synesthesia-menu_zoomout");
+// var imageFlexbox = document.querySelector(".synesthesia-flexbox");
+// var flexboxColumn = document.querySelectorAll(".synesthesia-flexbox_column");
+
+// plusButton.addEventListener("click", oneImageView);
+// minusButton.addEventListener("click", twoImageView);
+
+// function oneImageView() {
+//   for (var i = 0; i < flexboxColumn.length; i++) {
+//     flexboxColumn[i].style.flex = "100%";
+//     flexboxColumn[i].style.maxWidth = "100%";
+//     flexboxColumn[i].style.padding = "0px";
+//   }
+// }
+
+// function twoImageView() {
+//   for(var i = 0; i < flexboxColumn.length; i++) {
+//     flexboxColumn[i].style.flex = null;
+//     flexboxColumn[i].style.maxWidth = null;
+//     flexboxColumn[i].style.padding = null;
+//   }
+// }
+
 var plusButton = document.querySelector(".synesthesia-menu_zoomin");
 var minusButton = document.querySelector(".synesthesia-menu_zoomout");
-var imageFlexbox = document.querySelector(".synesthesia-flexbox");
+var imageFlexbox = document.querySelectorAll(".synesthesia-flexbox_pictures");
 var flexboxColumn = document.querySelectorAll(".synesthesia-flexbox_column");
 
-plusButton.addEventListener("click", oneImageView);
-minusButton.addEventListener("click", twoImageView);
+
+plusButton.addEventListener('click',() => {    
+  oneImageView();
+  oneImageViewFlexbox();    
+});
+
+minusButton.addEventListener('click',() => {    
+  twoImageView();
+  twoImageViewFlexbox();    
+});
 
 function oneImageView() {
   for (var i = 0; i < flexboxColumn.length; i++) {
@@ -14,6 +46,13 @@ function oneImageView() {
   }
 }
 
+function oneImageViewFlexbox() {
+  for (var i = 0; i < imageFlexbox.length; i++) {
+    imageFlexbox[i].style.flexFlow = "column nowrap";
+  }
+}
+
+
 function twoImageView() {
   for(var i = 0; i < flexboxColumn.length; i++) {
     flexboxColumn[i].style.flex = null;
@@ -21,3 +60,11 @@ function twoImageView() {
     flexboxColumn[i].style.padding = null;
   }
 }
+
+function twoImageViewFlexbox() {
+  for (var i = 0; i < imageFlexbox.length; i++) {
+    imageFlexbox[i].style.flexFlow = null;
+  }
+}
+
+
